b561f76b2684b5feb07f9bd5edafda5acdd4f87d
jhgaog <731746659@qq.com>
2021-03-01 13:45:13 +0800

    Bump 1.0.19

--- test/fixtures/model/client.go
+++ test/fixtures/model/client.go
@@ -47,6 +47,14 @@ type MyModel struct {
   ModuleMap map[string]*source.Client `json:"moduleMap,omitempty" xml:"moduleMap,omitempty" require:"true"`
   Object map[string]interface{} `json:"object,omitempty" xml:"object,omitempty" require:"true"`
   Numberfield *int `json:"numberfield,omitempty" xml:"numberfield,omitempty" require:"true"`
+  Int64field *int64 `json:"int64field,omitempty" xml:"int64field,omitempty" require:"true"`
+  Uint64field *uint64 `json:"uint64field,omitempty" xml:"uint64field,omitempty" require:"true"`
+  Int32field *int32 `json:"int32field,omitempty" xml:"int32field,omitempty" require:"true"`
+  Uint32field *uint32 `json:"uint32field,omitempty" xml:"uint32field,omitempty" require:"true"`
+  Int16field *int16 `json:"int16field,omitempty" xml:"int16field,omitempty" require:"true"`
+  Uint16field *uint16 `json:"uint16field,omitempty" xml:"uint16field,omitempty" require:"true"`
+  Int8field *int8 `json:"int8field,omitempty" xml:"int8field,omitempty" require:"true"`
+  Uint8field *uint8 `json:"uint8field,omitempty" xml:"uint8field,omitempty" require:"true"`
   Readable io.Reader `json:"readable,omitempty" xml:"readable,omitempty" require:"true"`
   Request *tea.Request `json:"request,omitempty" xml:"request,omitempty" require:"true"`
   Lists [][]*string `json:"lists,omitempty" xml:"lists,omitempty" require:"true" type:"Repeated"`
@@ -114,6 +122,46 @@ func (s *MyModel) SetObject(v map[string]interface{}) *MyModel {
 
 func (s *MyModel) SetNumberfield(v int) *MyModel {
   s.Numberfield = &v
+  return s
+}
+
+func (s *MyModel) SetInt64field(v int64) *MyModel {
+  s.Int64field = &v
+  return s
+}
+
+func (s *MyModel) SetUint64field(v uint64) *MyModel {
+  s.Uint64field = &v
+  return s
+}
+
+func (s *MyModel) SetInt32field(v int32) *MyModel {
+  s.Int32field = &v
+  return s
+}
+
+func (s *MyModel) SetUint32field(v uint32) *MyModel {
+  s.Uint32field = &v
+  return s
+}
+
+func (s *MyModel) SetInt16field(v int16) *MyModel {
+  s.Int16field = &v
+  return s
+}
+
+func (s *MyModel) SetUint16field(v uint16) *MyModel {
+  s.Uint16field = &v
+  return s
+}
+
+func (s *MyModel) SetInt8field(v int8) *MyModel {
+  s.Int8field = &v
+  return s
+}
+
+func (s *MyModel) SetUint8field(v uint8) *MyModel {
+  s.Uint8field = &v
   return s
 }
 
